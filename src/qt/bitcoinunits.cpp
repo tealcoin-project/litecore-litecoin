8b7915308a585620320be41cb4529c0b60de7455
tealcoin-project <tealcoin.project@gmail.com>
2018-04-11 00:25:06 +0700

    Rebrand coin symbol

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("TEAL");
+    case mBTC: return QString("mTEAL");
+    case uBTC: return QString::fromUtf8("μTEAL");
     default: return QString("???");
     }
 }
